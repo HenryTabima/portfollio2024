f290c960f311de46fc85d5f16f71c186f8fcb12d
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2024-09-22 12:06:48 -0500

    removed prop video

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -15,7 +15,9 @@ type Metadata = {
 };
 
 function getMDXFiles(dir: string) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+  return fs
+    .readdirSync(dir)
+    .filter((file: string) => path.extname(file) === ".mdx");
 }
 
 export async function markdownToHTML(markdown: string) {
